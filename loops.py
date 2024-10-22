b6819733cb6dbfff7e832a6e0c08ea623092f80b
Anant Tiwari <ananttwr28@gmail.com>
2024-10-22 12:14:05 +0530

    adding Q17-20 of Loops

--- loops.py
+++ loops.py
@@ -100,22 +100,77 @@ for i in range(2, number+1):
     
 '''
 
+# 22-oct-24 11:10 AM 
+
 # ------------ Q7 Write a program to print the Pascal’s triangle.
 
 '''
+
 def fact(n):
-    i, prod = 1, 1
-    while i != n:
+    if n == 0:
+        return 1
+    else:
+        i, prod = 1, 1
+        while i <= n:
+    
+            prod *= i
+            i += 1
+        return prod
 
-        prod *= i
-        i += 1
-    return prod
+n = int(input("Enter the number of rows: "))
 
-n = int(input())
-r = n
 for i in range(0, n):
-    for j in range(0, n):
+    for j in range(0, i+1):
+        o_p = (fact(i)) // (fact(i-j) * fact(j))
+        print(o_p, end = " ")
+    print()
 
-        print((fact(n)) / (fact(n-r) * fact(r)))
+
+'''
+
+# ------------ Q8 Write a program to check if a year is a leap year.
+
+'''
+
+year = int(input("Enter the year to be checked: "))
+
+if (year % 4) == 0 and (year % 100) != 0: 
+    print("Leap year")
+elif (year % 100) == 0 and (year % 400) == 0: 
+    print("Leap year")
+else:
+    print("Not a Leap year")
+
+'''
+
+# ------------ Q9 Write a program to print the sum of even and odd numbers in a given range.
+
+'''
+
+lower_bound = int(input("Enter the lower bound of the range: "))
+upper_bound = int(input("Enter the upper bound of the range: "))
+
+sum_even, sum_odd = 0, 0
+
+for i in range(lower_bound, upper_bound+1):
+    if (i % 2) == 0:
+        sum_even += i
+    else:
+        sum_odd += i
+
+print("Sum of even numbers is: ", sum_even)
+print("Sum of odd numbers is: ", sum_odd)
+
+'''
+
+# ------------ Q10 Write a program to print a pyramid pattern using stars.
+
+'''
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print("*", end = " ")       # end = " " to print on the same line
+    print()                         # to print on next line after one row ends
 
 '''
